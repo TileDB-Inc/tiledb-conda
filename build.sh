fb056b72b560fdf3fd7f9ca2f30a0ec5ed4dfc63
Jake Bolewski <jakebolewski@gmail.com>
2017-05-30 14:28:11 -0400

    don't assume GNU make

--- build.sh
+++ build.sh
@@ -3,6 +3,4 @@ NCPU=`sysctl -n hw.ncpu`
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_BUILD_TYPE=Release ..
-make -j${NCPU}
-# make check  [TODO: Build GMock]
-make install
+cmake --build . --target install -- -j${NCPU}
