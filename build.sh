ba395981608af9bb19f7015a3f3a245a38698ebd
Jake Bolewski <jakebolewski@gmail.com>
2017-06-09 13:01:43 -0600

    fix mac specific bits

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-NCPU=`sysctl -n hw.ncpu`
+if [ "$(uname)" == "Darwin" ]; then
+    NCPU=`sysctl -n hw.logicalcpu`
+fi
+if [ "$(uname)" == "Linux" ]; then
+    NCPU=`nproc --all`
+fi
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_BUILD_TYPE=Release ..
